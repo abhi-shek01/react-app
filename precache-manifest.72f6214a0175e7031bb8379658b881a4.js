f890c0d28a40bd4a62ed9ec52f34257f238f6ca2
abhi19shek <60101249+abhi19shek@users.noreply.github.com>
2020-06-05 19:09:28 +0530

    Updates

---
+++ precache-manifest.72f6214a0175e7031bb8379658b881a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bd1350e0da0d140cb00f04cf3a304d1",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "cf223ce2c56821ee5ce3",
+    "url": "/react-app/static/css/main.06a5c9ed.chunk.css"
+  },
+  {
+    "revision": "bcf56ddd0f0db81c957e",
+    "url": "/react-app/static/js/2.0d04d32e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-app/static/js/2.0d04d32e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf223ce2c56821ee5ce3",
+    "url": "/react-app/static/js/main.5da0f247.chunk.js"
+  },
+  {
+    "revision": "f850b7b266c5df6eb2c6",
+    "url": "/react-app/static/js/runtime-main.de8633f4.js"
+  }
+]);
